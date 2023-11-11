6ed110455ceec0dca1de82c1af7d356b13b1247e
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-11-11 16:11:28 -0300

    Estrutura do componente Topo

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import Banner from "../../../public/assets/banner.png";
+import Image from "next/image";
+import Banner from "../../../public/assets/banner.png"
 
 
 export default function Topo () {
     return (
-        <div>
-            <div className="secao-topo">
-                <img src={Banner}/>
+        <header>
+            <Image src={Banner} alt="banner"/>
+            <div>
                 <h1>Restaurante</h1>
+                <p>
+                    De pratos clássicos a criações surpreendentes, nosso cardápio é um
+                    requinte de sabores refinados.
+                </p>
             </div>
-        </div>
+        </header>
     );
 }
